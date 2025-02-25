e27cb020fa7c6b55d90f3e8b55f2fec679d6db2a
Ranvijay Singh <kuranvijay4@gmail.com>
2025-02-25 23:06:18 +0530

    pagination

--- Pagination/src/components/Fetch_data.jsx
+++ Pagination/src/components/Fetch_data.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import { AiFillLike } from "react-icons/ai";
+import { AiFillDislike } from "react-icons/ai";
 
 function Fetch_data() {
    //  const [todo, setTodo] = useState([]);
-    const [data, setdata] = useState(null);
+    const [data, setdata] = useState([]);
     // const fethc_product = async() =>{
     //    const res=await fetch('https://dummyjson.com/todos')
     //    const data=await res.json();
@@ -17,7 +19,7 @@ function Fetch_data() {
     // }, []);
     
     useEffect(() => {
-      axios.get('https://picsum.photos/').then((res)=> console.log(res));
+      axios.get('https://dummyjson.com/posts').then((res)=> setdata(res.data.posts));
     }, [])
     
     
@@ -30,8 +32,29 @@ function Fetch_data() {
             <h1>{todo.completed ? "True" :'false' }</h1>
             </div>
         })} */}
+       <div className="main">        { data.map((post)=>{
+          return (
+           <div key={post.id} className="post">
+            <h1>{post.id+"."}{post.title}</h1>
+            <div className='body'>
+            <p>{post.body}</p>
+            <div className='reactions'>
+            <button className='like'><AiFillLike/>{post.reactions.likes}</button>
+            <button className='dislike'><AiFillDislike/>{post.reactions.dislikes}</button>
+            </div>
+            </div>
+          </div>
+          )
+        })}
 
-        {data && <img src={data} alt="Random" />}
+          {/* {data &&<> <h3>Title:{data.title}</h3>
+            <p>body: {data.body}</p>
+            <h4>tags:{data.tags+" "}</h4>
+            {/* <h5>Likes:{data.reactions.likes}</h5>
+            <h5>desl  ikes:{data.reactions.dislikes}</h5> 
+           </>
+          } */}
+    </div>
     </div>
   )
 }
